e8201e4e837bf45a300b3ad723fe1ad3a49b40fd
Sunjay <sdsunjay73@yahoo.com>
2019-08-26 12:24:26 -0700

    Small reformatting - there are some errors still - WIP

--- final-assignment/src/components/Burger/Burger.js
+++ final-assignment/src/components/Burger/Burger.js
@@ -4,12 +4,19 @@ import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
   // turns object into array of key value pairs
-  const transformedIngredients = Object.keys(props.ingredients)
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
-      return [...Array(props.ingredients[igKey])].map( (_, i) => {
+      return [...Array( props.ingredients[igKey] )].map( (_, i) => {
         return <BurgerIngredient key={igKey + i} type={igKey} />
-      }); // [0,1]
-    });
+      } ); // [0,1]
+    } )
+    .reduce((arr, el) => {
+      return arr.concat(el);
+    }, []);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Please start adding ingredients</p>
+  }
+  //console.log(transformedIngredients);
   return (
     <div className={classes.Burger}>
       <BurgerIngredient type="bread-top" />
