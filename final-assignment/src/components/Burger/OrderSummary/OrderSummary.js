7adbdd0b8bc3e909b62758c0994d1aefbc4d754c
Sunjay <sdsunjay73@yahoo.com>
2019-08-26 16:30:23 -0700

    Adding Price to the the Order Summary

--- final-assignment/src/components/Burger/OrderSummary/OrderSummary.js
+++ final-assignment/src/components/Burger/OrderSummary/OrderSummary.js
@@ -19,7 +19,9 @@ const orderSummary = (props) => {
       <p>A burger with the following ingredients: </p>
       <ul>
         {ingredientSummary}
+
       </ul>
+      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
       <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
       <Button btnType="Success" clicked={props.purchaseContinueHandler}>Continue</Button>
--- final-assignment/src/containers/BurgerBuilder/BurgerBuilder.js
+++ final-assignment/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -96,6 +96,7 @@ class BurgerBuilder extends Component {
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
           <OrderSummary
             ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
             purchaseCancelled={this.purchaseCancelHandler}
             purchaseContinueHandler={this.purchaseContinuedHandler}
             />
