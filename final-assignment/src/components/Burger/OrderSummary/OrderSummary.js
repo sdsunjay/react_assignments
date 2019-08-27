c754efe14b1bb1e35c00dd913854a17901211173
Sunjay <sdsunjay73@yahoo.com>
2019-08-27 12:49:32 -0700

    Changed to a class-based component

--- final-assignment/src/components/Burger/OrderSummary/OrderSummary.js
+++ final-assignment/src/components/Burger/OrderSummary/OrderSummary.js
@@ -1,33 +1,39 @@
-import React from 'react';
-import Aux from '../../../hoc/Aux';
+import React, {Component} from 'react';
+import Aux from '../../../hoc/Aux/Aux';
 import PropTypes from 'prop-types';
 import Button from '../../UI/Button/Button'
 
+class OrderSummary extends Component {
 
-const orderSummary = (props) => {
-  const ingredientSummary = Object.keys(props.ingredients)
-    .map( igKey => {
-      return (
-      <li key={igKey}>
-        <span style={ {textTransform: 'capitalize'} }>{igKey}</span> : {props.ingredients[igKey]}
-      </li> );
-    });
-  //<li>Salad: 1 </li>
-  return (
+  // this could be a functional component, doesn't need to be a class
+  componentDidUpdate() {
+    console.log('[OrderSummary] componentDidUpdate');
+  }
+
+  render() {
+
+    const ingredientSummary = Object.keys(this.props.ingredients)
+      .map( igKey => {
+        return (
+        <li key={igKey}>
+          <span style={ {textTransform: 'capitalize'} }>{igKey}</span> : {this.props.ingredients[igKey]}
+        </li> );
+      });
+
+    return (
       <Aux>
       <h3>Your Order</h3>
       <p>A burger with the following ingredients: </p>
       <ul>
         {ingredientSummary}
-
       </ul>
-      <p><strong>Total Price: {props.price.toFixed(2)}</strong></p>
+      <p><strong>Total Price: {this.props.price.toFixed(2)}</strong></p>
       <p>Continue to Checkout?</p>
-      <Button btnType="Danger" clicked={props.purchaseCancelled}>Cancel</Button>
-      <Button btnType="Success" clicked={props.purchaseContinueHandler}>Continue</Button>
+      <Button btnType="Danger" clicked={this.props.purchaseCancelled}>Cancel</Button>
+      <Button btnType="Success" clicked={this.props.purchaseContinueHandler}>Continue</Button>
       </Aux>
-  );
+    );
+  }
+}
 
-};
-
-export default orderSummary;
+export default OrderSummary;
