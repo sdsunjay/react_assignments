5e63281c97b46d62061c8c4866e0304094b535d7
Sunjay <sdsunjay73@yahoo.com>
2019-08-27 17:06:25 -0700

    Adding Interceptors to Execute Code Globally

--- section-9-assignment/src/index.js
+++ section-9-assignment/src/index.js
@@ -3,6 +3,30 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import axios from 'axios';
+
+// applied to ALL requests globally
+axios.interceptors.request.use(request => {
+  console.log(request);
+  // Edit the request config
+  // such as add headers
+  return request;
+}, error => {
+  // if you have no internet connectivity
+  console.log(error);
+  return Promise.reject(error);
+});
+
+axios.interceptors.response.use(response => {
+  console.log(response);
+  // Edit the request config
+  // such as add headers
+  return response;
+}, error => {
+  // if you have no internet connectivity
+  console.log(error);
+  return Promise.reject(error);
+});
 
 ReactDOM.render( <App />, document.getElementById( 'root' ) );
 registerServiceWorker();
