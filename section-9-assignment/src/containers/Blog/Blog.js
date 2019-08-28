dbe7df45566d6c64dd66902cb6f50925b23a91df
Sunjay <sdsunjay73@yahoo.com>
2019-08-28 15:45:54 -0700

    Switching between pages

--- section-9-assignment/src/containers/Blog/Blog.js
+++ section-9-assignment/src/containers/Blog/Blog.js
@@ -4,12 +4,9 @@ import { Route } from 'react-router-dom';
 
 import './Blog.module.css';
 import Posts from './Posts/Posts';
+import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
-
-    constructor(props) {
-      super(props);
-    }
 
     render () {
       return (
@@ -24,6 +21,7 @@ class Blog extends Component {
           </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>}/>*/}
         <Route path="/" exact component={Posts}/>
+        <Route path="/new-post" exact component={NewPost}/>
         </div>
       );
     }
