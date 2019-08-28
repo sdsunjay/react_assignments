679e058c859af1da0eaee269c5ba6a31558a1a6d
Sunjay <sdsunjay73@yahoo.com>
2019-08-28 11:02:54 -0700

    Handling Errors

--- final-assignment/src/containers/BurgerBuilder/BurgerBuilder.js
+++ final-assignment/src/containers/BurgerBuilder/BurgerBuilder.js
@@ -8,6 +8,7 @@ import BuildControls from '../../components/Burger/BuildControls/BuildControls'
 import Modal from '../../components/UI/Modal/Modal';
 import axios from '../../axios-orders';
 import Spinner from '../../components/UI/Spinner/Spinner';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -101,7 +102,7 @@ class BurgerBuilder extends Component {
       },
       deliveryMethod: 'fastest'
     }
-    axios.post('orders.json', order)
+    axios.post('/orders.json', order)
     .then(response => {
         this.setState({loading: false, purchasing: false});
       })
@@ -149,4 +150,4 @@ class BurgerBuilder extends Component {
 
 }
 
-export default BurgerBuilder;
+export default withErrorHandler(BurgerBuilder, axios);
--- final-assignment/src/hoc/withErrorHandler/withErrorHandler.js
+++ final-assignment/src/hoc/withErrorHandler/withErrorHandler.js
@@ -1,19 +1,45 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Modal from '../../components/UI/Modal/Modal';
 import Aux from '../Aux/Aux';
 
-const withErrorHandler = (WrappedComponent) => {
-  return (props) => {
-    return (
-      <Aux>
-        <Modal>
-        Something did not work!
-        </Modal>
-      </Aux>
-      <WrappedComponent {... props} />
-    );
-  }
+const withErrorHandler = ( WrappedComponent, axios ) => {
+    return class extends Component {
+
+        constructor(props) {
+          super(props);
+          this.state = {
+              error: null
+          }
+        }
+
+        componentWillMount () {
+            axios.interceptors.request.use(request => {
+                this.setState({error: null});
+                return request;
+            });
+            axios.interceptors.response.use(res => res, error => {
+                this.setState({error: error});
+            });
+        }
+
+        errorConfirmedHandler = () => {
+            this.setState({error: null});
+        }
+
+        render () {
+            return (
+                <Aux>
+                    <Modal
+                        show={this.state.error}
+                        modalClosed={this.errorConfirmedHandler}>
+                        {this.state.error ? this.state.error.message : null}
+                    </Modal>
+                    <WrappedComponent {...this.props} />
+                </Aux>
+            );
+        }
+    }
 }
 
 export default withErrorHandler;
