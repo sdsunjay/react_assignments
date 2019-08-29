f6ece98cedbc00e295a1b206272ebe01e4378f59
Sunjay <sdsunjay73@yahoo.com>
2019-08-29 11:31:35 -0700

    Formatting

--- section-9-assignment/src/containers/Blog/Blog.js
+++ section-9-assignment/src/containers/Blog/Blog.js
@@ -8,37 +8,43 @@ import NewPost from './NewPost/NewPost';
 
 class Blog extends Component {
 
-    render () {
-      return (
-        <div className="Blog">
-          <header>
-            <nav>
-              <ul>
-                <li><NavLink
-                      to="/posts/"
-                      exact
-                      activeClassName="my-active"
-                      activeStyle={{
-                        color: '#fa923f',
-                        textDecoration: 'underline'
-                      }}>Posts</NavLink></li>
-                <li><NavLink to={{
-                      pathname: '/new-post',
-                      hash: '#submit',
-                      search: '?quick-submit=true'
-                }}>New Post</NavLink></li>
-              </ul>
-           </nav>
-          </header>
+  constructor(props) {
+    super(props);
+    this.state = {
+      loadedPost: null
+    }
+  }
+  render () {
+    return (
+      <div className="Blog">
+        <header>
+          <nav>
+            <ul>
+              <li><NavLink
+                to="/posts/"
+                exact
+                activeClassName="my-active"
+                activeStyle={{
+                  color: '#fa923f',
+                  textDecoration: 'underline'
+                }}>Posts</NavLink></li>
+              <li><NavLink to={{
+                pathname: '/new-post',
+                hash: '#submit',
+                search: '?quick-submit=true'
+              }}>New Post</NavLink></li>
+            </ul>
+          </nav>
+        </header>
         {/* <Route path="/" exact render={() => <h1>Home</h1>}/>*/}
           <Switch>
             <Route path="/new-post" exact component={NewPost}/>
             <Route path="/posts" component={Posts}/>
             <Redirect from="/" to="/posts" />
           </Switch>
-        </div>
-      );
-    }
+      </div>
+    );
+  }
 }
 
 export default Blog;
