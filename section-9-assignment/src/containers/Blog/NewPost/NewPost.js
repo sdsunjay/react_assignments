fba47750d8903236c21f7962c53b4cf862d785a1
Sunjay <sdsunjay73@yahoo.com>
2019-08-29 11:14:09 -0700

    Formatting

--- section-9-assignment/src/containers/Blog/NewPost/NewPost.js
+++ section-9-assignment/src/containers/Blog/NewPost/NewPost.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 
 import './NewPost.module.css';
 
@@ -26,21 +27,22 @@ class NewPost extends Component {
     }
 
     render () {
-        return (
-            <div className="NewPost">
-                <h1>Add a Post</h1>
-                <label>Title</label>
-                <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
-                <label>Content</label>
-                <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
-                <label>Author</label>
-                <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
-                    <option value="Max">Max</option>
-                    <option value="Manu">Manu</option>
-                </select>
-                <button onClick={this.postPostHandler}>Add Post</button>
-            </div>
-        );
+      return (
+        <div className="NewPost">
+          <Redirect to="/posts/" />
+          <h1>Add a Post</h1>
+          <label>Title</label>
+          <input type="text" value={this.state.title} onChange={(event) => this.setState({title: event.target.value})} />
+          <label>Content</label>
+          <textarea rows="4" value={this.state.content} onChange={(event) => this.setState({content: event.target.value})} />
+          <label>Author</label>
+          <select value={this.state.author} onChange={(event) => this.setState({author: event.target.value})}>
+            <option value="Max">Max</option>
+            <option value="Manu">Manu</option>
+          </select>
+          <button onClick={this.postPostHandler}>Add Post</button>
+        </div>
+      );
     }
 }
 
