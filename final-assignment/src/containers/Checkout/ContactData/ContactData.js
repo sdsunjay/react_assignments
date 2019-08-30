548d86da1b187e2e1e380008634363a06a765a34
Sunjay <sdsunjay73@yahoo.com>
2019-08-30 16:46:59 -0700

    Handling user input

--- final-assignment/src/containers/Checkout/ContactData/ContactData.js
+++ final-assignment/src/containers/Checkout/ContactData/ContactData.js
@@ -9,8 +9,8 @@ class ContactData extends Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      orderForm : {
+      this.state = {
+        orderForm : {
         name: {
           elementType: 'input',
           elementConfig: {
@@ -51,37 +51,51 @@ class ContactData extends Component {
           },
           value: ''
         },
-        deliveryMethod: {elementType: 'select'},
-        elementConfig: {
-          options: [
-            {value: 'fastest', displayValue: 'Fastest'},
-            {value: 'cheapest', displayValue: 'Cheapest'}
-          ]
-        },
-        value: ''
+        deliveryMethod: {
+          elementType: 'select',
+          elementConfig: {
+            options: [
+              {value: 'fastest', displayValue: 'Fastest'},
+              {value: 'cheapest', displayValue: 'Cheapest'}
+            ]
+          },
+          value: ''
       },
       loading: false
+      }
     }
   }
 
   orderHandler = (event) => {
     event.preventDefault();
     this.setState({loading: true});
-      //alert('you continue!');
-      const order = {
-        ingredients: this.props.ingredients,
-        // we should check this price on the server, dont want it manipulated
-        price: this.props.price
-      }
-      axios.post('/orders.json', order)
-      .then(response => {
-          this.setState({loading: false});
-          this.props.history.push('/');
-        })
-      .catch(error => {
-          this.setState({loading: false});
-        });
-      }
+    //alert('you continue!');
+    const order = {
+      ingredients: this.props.ingredients,
+      // we should check this price on the server, dont want it manipulated
+      price: this.props.price
+    }
+    axios.post('/orders.json', order)
+    .then(response => {
+      this.setState({loading: false});
+      this.props.history.push('/');
+    })
+    .catch(error => {
+      this.setState({loading: false});
+    });
+  }
+
+  inputChangedHandler = (event, inputIdentifier) => {
+    const updatedOrderForm = {
+      ...this.state.orderForm
+    }
+    const updatedFormElement = {
+      ...updatedOrderForm[inputIdentifier]
+  }
+  updatedFormElement.value = event.target.value;
+  updatedOrderForm[inputIdentifier] = updatedFormElement;
+  this.setState({orderForm: updatedOrderForm});
+}
 
   render () {
 
@@ -100,7 +114,8 @@ class ContactData extends Component {
               key={formElement.id}
               elementType={formElement.config.elementType}
               elementConfig={formElement.config.elementConfig}
-              value={formElement.config.value} />
+              value={formElement.config.value}
+              changed={(event) => this.inputChangedHandler(event, formElement.id)} />
             )
           )}
           <Button btntype="Success" clicked={this.orderHandler}>ORDER</Button>
