e1f35f95b126c58ff60ba08e936c230c42181b23
Sunjay <sdsunjay73@yahoo.com>
2019-08-31 19:08:44 -0700

    form validation

--- final-assignment/src/containers/Checkout/ContactData/ContactData.js
+++ final-assignment/src/containers/Checkout/ContactData/ContactData.js
@@ -10,60 +10,90 @@ class ContactData extends Component {
   constructor(props) {
     super(props);
       this.state = {
-        orderForm : {
-        name: {
-          elementType: 'input',
-          elementConfig: {
-            type: 'text',
-            placeholder: 'Your Name'
-          },
-          value: ''
-        },
-        address: {
-          elementType: 'input',
-          elementConfig: {
-            type: 'text',
-            placeholder: 'Your Address'
-          },
-          value: ''
-        },
-        zip: {
-          elementType: 'input',
-          elementConfig: {
-            type: 'text',
-            placeholder: 'Your Zipcode'
-          },
-          value: ''
-        },
-        state: {
-          elementType: 'input',
-          elementConfig: {
-            type: 'text',
-            placeholder: 'Your State'
-          },
-          value: ''
-        },
-        email: {
-          elementType: 'input',
-          elementConfig: {
-            type: 'email',
-            placeholder: 'Your Email'
-          },
-          value: ''
-        },
-        deliveryMethod: {
-          elementType: 'select',
-          elementConfig: {
-            options: [
-              {value: 'fastest', displayValue: 'Fastest'},
-              {value: 'cheapest', displayValue: 'Cheapest'}
-            ]
-          },
-          value: ''
-      },
-      loading: false
-      }
-    }
+             orderForm: {
+                 name: {
+                     elementType: 'input',
+                     elementConfig: {
+                         type: 'text',
+                         placeholder: 'Your Name'
+                     },
+                     value: '',
+                     validation: {
+                       required: true
+                     },
+                     valid: false,
+                     touched: false
+                 },
+                 street: {
+                     elementType: 'input',
+                     elementConfig: {
+                         type: 'text',
+                         placeholder: 'Street'
+                     },
+                     value: '',
+                     validation: {
+                       required: true
+                     },
+                     valid: false,
+                     touched: false
+                 },
+                 zipCode: {
+                     elementType: 'input',
+                     elementConfig: {
+                         type: 'text',
+                         placeholder: 'ZIP Code'
+                     },
+                     value: '',
+                     validation: {
+                       required: true,
+                       minLength: 5,
+                       maxLength: 5
+                     },
+                     valid: false,
+                     touched: false
+                 },
+                 country: {
+                     elementType: 'input',
+                     elementConfig: {
+                         type: 'text',
+                         placeholder: 'Country'
+                     },
+                     value: '',
+                     validation: {
+                       required: true
+                     },
+                     valid: false,
+                     touched: false
+                 },
+                 email: {
+                     elementType: 'input',
+                     elementConfig: {
+                         type: 'email',
+                         placeholder: 'Your E-Mail'
+                     },
+                     value: '',
+                     validation: {
+                       required: true
+                     },
+                     valid: false,
+                     touched: false
+                 },
+                 deliveryMethod: {
+                     elementType: 'select',
+                     elementConfig: {
+                         options: [
+                             {value: 'fastest', displayValue: 'Fastest'},
+                             {value: 'cheapest', displayValue: 'Cheapest'}
+                         ]
+                     },
+                     validation: {},
+                     value: 'fastest',
+                     valid: true
+                 }
+             },
+             loading: false,
+             formIsValid: false
+         }
   }
 
   orderHandler = (event) => {
@@ -89,6 +119,21 @@ class ContactData extends Component {
     });
   }
 
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if(rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if(rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if(rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
+
   inputChangedHandler = (event, inputIdentifier) => {
     const updatedOrderForm = {
       ...this.state.orderForm
@@ -97,8 +142,15 @@ class ContactData extends Component {
       ...updatedOrderForm[inputIdentifier]
   }
   updatedFormElement.value = event.target.value;
+  updatedFormElement.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+  updatedFormElement.touched = true;
+
+  let formIsValid = true;
+  for(let inputIdentifier in updatedOrderForm) {
+    formIsValid = updatedOrderForm[inputIdentifier].valid && formIsValid
+  }
   updatedOrderForm[inputIdentifier] = updatedFormElement;
-  this.setState({orderForm: updatedOrderForm});
+  this.setState({orderForm: updatedOrderForm, formIsValid: formIsValid});
 }
 
   render () {
@@ -119,10 +171,15 @@ class ContactData extends Component {
               elementType={formElement.config.elementType}
               elementConfig={formElement.config.elementConfig}
               value={formElement.config.value}
+              invalid={!formElement.config.valid}
+              shouldValidate={formElement.config.validation}
+              touched={formElement.config.touched}
               changed={(event) => this.inputChangedHandler(event, formElement.id)} />
-            )
-          )}
-          <Button btntype="Success">ORDER</Button>
+          ))}
+          <Button
+            btnType="Success"
+            disable={!this.state.formIsValid}>
+            ORDER</Button>
         </form>
         );
     if(this.state.loading) {
@@ -136,5 +193,4 @@ class ContactData extends Component {
     );
   }
 }
-
 export default ContactData;
