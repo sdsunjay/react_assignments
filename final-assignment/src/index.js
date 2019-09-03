41bf4c971497fcd1a25a7ec13af2e15211cee85b
Sunjay <sdsunjay73@yahoo.com>
2019-09-03 14:22:39 -0700

    Add Redux

--- final-assignment/src/index.js
+++ final-assignment/src/index.js
@@ -2,15 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
+import { createStore } from 'redux';
+import reducer from './store/reducer';
+import { Provider } from 'react-redux';
+
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const store = createStore(reducer);
+
 const app = (
+  <Provider store={store}>
   <BrowserRouter>
     <App />
   </BrowserRouter>
-)
+  </Provider>
+);
 ReactDOM.render(app, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
