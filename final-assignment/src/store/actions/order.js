476975c12eb744e467cd6e6710bfe520844536f7
Sunjay <sdsunjay73@yahoo.com>
2019-09-04 14:14:52 -0700

    Combining Reducers

--- final-assignment/src/store/actions/order.js
+++ final-assignment/src/store/actions/order.js
@@ -28,7 +28,7 @@ export const purchaseBurger = (orderData) => {
     dispatch(purchaseBurgerStart());
     axios.post('/orders.json', orderData)
     .then(response => {
-      dispatch(purchaseBurgerSuccess(response.data.orders.id, orderData))
+      dispatch(purchaseBurgerSuccess(response.data.name, orderData))
     } )
     .catch(error => {
       dispatch( purchaseBurgerFail(error) );
