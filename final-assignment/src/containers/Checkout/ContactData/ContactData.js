4d2692b5ec90130555c3c450e877f89cd806d4da
Sunjay <sdsunjay73@yahoo.com>
2019-09-04 13:33:47 -0700

    Working on Order Actions

--- final-assignment/src/containers/Checkout/ContactData/ContactData.js
+++ final-assignment/src/containers/Checkout/ContactData/ContactData.js
@@ -95,7 +95,6 @@ class ContactData extends Component {
                      valid: true
                  }
              },
-             loading: false,
              formIsValid: false
          }
   }
@@ -183,7 +182,7 @@ class ContactData extends Component {
             ORDER</Button>
         </form>
         );
-    if(this.state.loading) {
+    if(this.props.loading) {
       form = <Spinner />;
     }
     return (
@@ -198,14 +197,15 @@ class ContactData extends Component {
 const mapStateToProps = state => {
   return {
     ings: state.ingredients,
-    price: state.totalPrice
+    price: state.totalPrice,
+    loading: state.loading
   };
 };
 
 const mapDispatchToProps = dispatch => {
   return {
-    onOrderBurger: (orderData) => dispatch(actions.purchaseBurgerStart(orderData))
-  }
+    onOrderBurger: (orderData) => dispatch(actions.purchaseBurger(orderData))
+  };
 };
 
 
