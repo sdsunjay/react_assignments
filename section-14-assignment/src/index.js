25a3d3c04100e174a4b5fb9a67c636757ae1eae0
Sunjay <sdsunjay73@yahoo.com>
2019-09-03 17:15:41 -0700

    Add 'reduxThunk'

--- section-14-assignment/src/index.js
+++ section-14-assignment/src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
+import ReduxThunk from 'redux-thunk';
 import counterReducer from './store/reducers/counter';
 import resultReducer from './store/reducers/result';
 import { Provider } from 'react-redux';
@@ -26,7 +27,7 @@ const logger = (store) => {
 };
 //const store = createStore(rootReducer, applyMiddleware(logger));
   const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-  const store = createStore(rootReducer,composeEnhancers(applyMiddleware(logger)));
+  const store = createStore(rootReducer,composeEnhancers(applyMiddleware(logger, ReduxThunk)));
 
 ReactDOM.render(<Provider store={store}><App /></Provider>, document.getElementById('root'));
 registerServiceWorker();
