c98daf8d766328ea470e954514a8f15e6ffbf520
Sunjay <sdsunjay73@yahoo.com>
2019-09-04 15:30:48 -0700

    Fetching Orders (via Redux)

--- final-assignment/src/store/actions/index.js
+++ final-assignment/src/store/actions/index.js
@@ -3,4 +3,8 @@ export {
   removeIngredient,
   initIngredients
 } from './burgerBuilder';
-export { purchaseBurger, purchaseInit } from './order'
+export {
+  purchaseBurger,
+  purchaseInit,
+  fetchOrders
+} from './order'
