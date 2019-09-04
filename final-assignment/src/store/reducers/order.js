894ca6e933dc14d85546d54fd3ad8c00891c88aa
Sunjay <sdsunjay73@yahoo.com>
2019-09-04 11:27:17 -0700

    Preparing folder structure

---
+++ final-assignment/src/store/reducers/order.js
@@ -0,0 +1,13 @@
+import * as actionTypes from '../action/actionTypes';
+
+
+// Reducer
+const reducer = (state = initialState, action) => {
+
+  switch(action.type) {
+    default:
+      return state;
+  }
+};
+
+export default order;
