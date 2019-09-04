82aecde1f79fe28e7c5c084b739eb34cf68a0fd9
Sunjay <sdsunjay73@yahoo.com>
2019-09-04 11:39:56 -0700

    Creating Action Creators

--- final-assignment/src/index.js
+++ final-assignment/src/index.js
@@ -3,14 +3,14 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 
 import { createStore } from 'redux';
-import reducer from './store/reducer';
+import burgerBuilderReducer from './store/reducers/burgerBuilder';
 import { Provider } from 'react-redux';
 
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const store = createStore(window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
+const store = createStore(burgerBuilderReducer, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__());
 const app = (
   <Provider store={store}>
   <BrowserRouter>
