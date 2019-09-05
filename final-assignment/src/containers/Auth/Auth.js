e985444348d5462e9ead8871fa9610acc4a26665
Sunjay <sdsunjay73@yahoo.com>
2019-09-05 11:34:30 -0700

    Adding Auth Form

---
+++ final-assignment/src/containers/Auth/Auth.js
@@ -0,0 +1,138 @@
+import React, { Component } from 'react';
+import Aux from '../../hoc/Aux/Aux';
+import { connect } from 'react-redux';
+
+import classes from './Auth.module.css';
+import Button from '../../components/UI/Button/Button';
+import axios from '../../axios-orders';
+import Spinner from '../../components/UI/Spinner/Spinner';
+import Input from '../../components/UI/Input/Input';
+import withErrorHandler from '../../hoc/withErrorHandler/withErrorHandler';
+
+class Auth extends Component {
+
+  constructor(props) {
+    super(props);
+      this.state = {
+             controls: {
+                 name: {
+                     elementType: 'input',
+                     elementConfig: {
+                         type: 'email',
+                         placeholder: 'Your Email'
+                     },
+                     value: '',
+                     validation: {
+                       required: true,
+                       isEmail: true
+                     },
+                     valid: false,
+                     touched: false
+                 },
+                 password: {
+                     elementType: 'input',
+                     elementConfig: {
+                         type: 'password',
+                         placeholder: 'Your Password'
+                     },
+                     value: '',
+                     validation: {
+                       required: true,
+                       minLength: 6
+                     },
+                     valid: false,
+                     touched: false
+                 }
+             },
+             formIsValid: false
+         }
+  }
+
+  checkValidity(value, rules) {
+    let isValid = true;
+
+    if(!rules) {
+      return true;
+    }
+    if(rules.required) {
+      isValid = value.trim() !== '' && isValid;
+    }
+    if(rules.minLength) {
+      isValid = value.length >= rules.minLength && isValid;
+    }
+    if(rules.maxLength) {
+      isValid = value.length <= rules.maxLength && isValid;
+    }
+    return isValid;
+  }
+
+  inputChangedHandler = (event, controlName) => {
+    const updatedControls = {
+      ...this.state.controls,
+      [controlName]: {
+        ...this.state.controls[controlName],
+        value: event.target.value,
+        valid: this.checkValidity(event.target.value, this.state.controls[controlName].validation),
+        touched: true
+      }
+    }
+      this.setState({ controls: updatedControls });
+    /**
+    const updatedFormElement = {
+      ...updatedControls[controlName]
+    }
+    updatedControls.value = event.target.value;
+    updatedControls.valid = this.checkValidity(updatedFormElement.value, updatedFormElement.validation);
+    //updatedFormElement.valid = true
+    updatedFormElement.touched = true;
+
+    let formIsValid = true;
+    for(let inputIdentifier in updatedOrderForm) {
+      formIsValid = updatedControls[controlName].valid && formIsValid
+    }
+    updatedOrderForm[controlName] = updatedFormElement;
+    this.setState({controls: updatedControls, formIsValid: formIsValid});
+    */
+  }
+
+  render () {
+
+    const formElementsArray = [];
+    for (let key in this.state.controls) {
+      formElementsArray.push({
+        id: key,
+        config: this.state.controls[key]
+      });
+    }
+
+    let form = (
+        <form>
+          {formElementsArray.map(formElement =>
+            (<Input
+              key={formElement.id}
+              elementType={formElement.config.elementType}
+              elementConfig={formElement.config.elementConfig}
+              value={formElement.config.value}
+              invalid={!formElement.config.valid}
+              shouldValidate={formElement.config.validation}
+              touched={formElement.config.touched}
+              changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+          ))}
+          <Button
+            btnType="Success"
+            disable={!this.state.formIsValid}>Submit</Button>
+        </form>
+        );
+    if(this.props.loading) {
+      form = <Spinner />;
+    }
+    return (
+      <div className={classes.Auth}>
+        <h4>Enter your Contact Data</h4>
+        {form}
+      </div>
+    );
+  }
+}
+
+export default Auth;
