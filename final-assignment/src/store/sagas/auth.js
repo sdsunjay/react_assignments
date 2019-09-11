aeab5479b0b05f1bee77c289c3fcdc2ff321cdf2
Sunjay <sdsunjay73@yahoo.com>
2019-09-11 16:18:17 -0700

    Hooking up the Saga

---
+++ final-assignment/src/store/sagas/auth.js
@@ -0,0 +1,12 @@
+import { put } from 'redux-saga/effects';
+
+import * as actionTypes from '../actions/actionTypes';
+
+export function* logoutSaga(action) {
+  yield localStorage.removeItem('token');
+  yield localStorage.removeItem('expirationDate');
+  yield localStorage.removeItem('localId');
+  yield put({
+      type: actionTypes.AUTH_LOGOUT
+    });
+}
