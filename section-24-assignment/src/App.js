dd2c6862878445e9f0dee39981f9fcb59f2bb2a9
Sunjay <sdsunjay73@yahoo.com>
2019-09-11 12:28:23 -0700

    Wrapping the Transition Component

--- section-24-assignment/src/App.js
+++ section-24-assignment/src/App.js
@@ -63,8 +63,8 @@ class App extends Component {
               ></div>
           )}
         </Transition>
-        {this.state.modalIsOpen ? <Modal show={this.state.modalIsOpen} closed={this.closeModal} /> : null}
-        {this.state.modalIsOpen ? <Backdrop show={this.state.modalIsOpen} /> : null}
+        <Modal show={this.state.modalIsOpen} closed={this.closeModal} />
+        {this.state.modalIsOpen ? <Backdrop show /> : null}
         <button className="Button" onClick={this.showModal}>Open Modal</button>
         <h3>Animating Lists</h3>
         <List />
