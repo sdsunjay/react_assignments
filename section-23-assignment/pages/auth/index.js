bc78f7c3cbe1f6f2c05fa850981fa4099fd3d275
Sunjay <sdsunjay73@yahoo.com>
2019-09-11 09:52:47 -0700

    Using 'getInitialProps'

--- section-23-assignment/pages/auth/index.js
+++ section-23-assignment/pages/auth/index.js
@@ -2,11 +2,21 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
   <div>
-    <h1>The Main Auth Page</h1>
+    <h1>The Auth Index Page - {props.appName}</h1>
     <User name="Max" age={28} />
   </div>
 );
+
+authIndexPage.getInitialProps = async function(context) {
+  const promise = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve( { appName: "Super App (Auth)"});
+    }, 100);
+  });
+  console.log(promise);
+  return promise;
+};
 
 export default authIndexPage;
