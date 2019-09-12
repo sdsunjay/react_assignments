4b22baa57772ec3acf9e8b12eb0bc1a2e19d8854
Sunjay <sdsunjay73@yahoo.com>
2019-09-12 16:58:02 -0700

    Added 'addIngredientHandler' and 'removeIngredientHandler'

--- section-26-assignment/src/components/Ingredients/Ingredients.js
+++ section-26-assignment/src/components/Ingredients/Ingredients.js
@@ -1,15 +1,30 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import IngredientForm from './IngredientForm';
+import IngredientList from './IngredientList';
 import Search from './Search';
 
-function Ingredients() {
+const Ingredients = () => {
+  const [ userIngredients, setUserIngredients ] = useState([]);
+
+
+  const addIngredientHandler = ingredient => {
+    setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}
+    ]);
+  };
+  const removeIngredientHandler = ingredientId => {
+    setUserIngredients(prevIngredients =>
+      prevIngredients.filter(ingredient => ingredient.id !== ingredientId)
+    );
+  };
+
   return (
     <div className="App">
-      <IngredientForm />
+      <IngredientForm onAddIngredient={addIngredientHandler} />
 
       <section>
         <Search />
+        <IngredientList ingredients={userIngredients} onRemoveItem={removeIngredientHandler}/>
         {/* Need to add list here! */}
       </section>
     </div>
