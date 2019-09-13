d11450a7fdc3f8509285a3474a9f1a55c01349f4
Sunjay <sdsunjay73@yahoo.com>
2019-09-13 09:44:51 -0700

    added 'useEffect()'

--- section-26-assignment/src/components/Ingredients/Ingredients.js
+++ section-26-assignment/src/components/Ingredients/Ingredients.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 import IngredientForm from './IngredientForm';
 import IngredientList from './IngredientList';
@@ -7,10 +7,37 @@ import Search from './Search';
 const Ingredients = () => {
   const [ userIngredients, setUserIngredients ] = useState([]);
 
+//gets executed right after and every render
+// the second argument is the dependencies
+  useEffect( () => {
+    fetch('https://react-hooks-update-2e06f.firebaseio.com/ingredients.json').then(response => {
+      return response.json();
+    }).then(responseData => {
+      const loadedIngredients = [];
+      for(const key in responseData) {
+        loadedIngredients.push({
+          id: key,
+          title: responseData[key].title,
+          amount: responseData[key].amount
+        })
+      }
+      setUserIngredients(loadedIngredients);
+    });
+  }, []);
+
+
 
   const addIngredientHandler = ingredient => {
-    setUserIngredients(prevIngredients => [...prevIngredients, {id: Math.random().toString(), ...ingredient}
-    ]);
+    fetch('https://react-hooks-update-2e06f.firebaseio.com/ingredients.json', {
+      method: 'POST',
+      body: JSON.stringify(ingredient),
+      headers: { 'Content-Type': 'application/json' }
+    }).then(response => {
+      return response.json();
+    }).then(responseData => {
+      setUserIngredients(prevIngredients => [...prevIngredients, {id: responseData.name, ...ingredient}
+      ]);
+    })
   };
   const removeIngredientHandler = ingredientId => {
     setUserIngredients(prevIngredients =>
